5dad17d1e33026f048da5098b509e9907c67a83a
teaRain9 <adel.ht@gmail.com>
2022-07-22 16:26:48 -0700

    no

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,11 +7,39 @@ import IngredientList from "./IngredientList";
 const Ingredients = () => {
     const [ingredients, setIngredients] = useState([])
 
+    useEffect(() => {
+        fetch('https://flamelinktest-b9226-default-rtdb.firebaseio.com/ingredients.json').then(
+            response => response.json()
+        ).then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+                loadedIngredients.push({
+                    id: key,
+                    title: responseData[key].title,
+                    amount: responseData[key].amount
+                });
+            }
+            setIngredients(loadedIngredients);
+        })
+    }, [])
+
+    const filteredIngredientHandler = useCallback( filteredIngredients => {
+        setIngredients(filteredIngredients)
+    }, [])
+
     const addIngredientHandler = ingredient => {
-        setIngredients(prevState => [
-            ...prevState,
-            {id: Math.random().toString(), ...ingredient}
-        ])
+        fetch('https://flamelinktest-b9226-default-rtdb.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify({title: ingredient.title, amount: ingredient.amount}),
+            headers: { 'Content-Type': 'application/json'}
+        }).then (response => {
+            return response.json();
+        }).then(responseData => {
+            setIngredients(prevState => [
+                ...prevState,
+                {id: responseData.name, ...ingredient} //the way that th id is assigned, that's just how firebase works
+            ]);
+        });
     }
 
     const removeIngredientHandler = id => {
@@ -24,7 +52,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientHandler}/>
         <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
@@ -32,3 +60,4 @@ const Ingredients = () => {
 }
 
 export default Ingredients;
+
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
-  return (
-    <section className="search">
-      <Card>
-        <div className="search-input">
-          <label>Filter by Title</label>
-          <input type="text" />
-        </div>
-      </Card>
-    </section>
-  );
+
+    const {onLoadIngredients} = props;
+
+    const [enteredFilter, setEnteredFilter] = useState('')
+
+
+    useEffect(() => {
+        const query = enteredFilter.length ===0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`
+        fetch('https://flamelinktest-b9226-default-rtdb.firebaseio.com/ingredients.json' + query)
+            .then(response => response.json())
+            .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+                loadedIngredients.push({
+                    id: key,
+                    title: responseData[key].title,
+                    amount: responseData[key].amount
+                });
+            }
+            onLoadIngredients(loadedIngredients)
+            console.log('aha')
+        })
+    }, [enteredFilter, onLoadIngredients])
+
+    return (
+        <section className="search">
+            <Card>
+                <div className="search-input">
+                    <label>Filter by Title</label>
+                    <input type="text"
+                           value={enteredFilter}
+                           onChange={event => setEnteredFilter(event.target.value)}
+                    />
+                </div>
+            </Card>
+        </section>
+    );
 });
 
 export default Search;
