2de4b4b77c9cab0476b283e2b74c50057f439a23
teaRain9 <adel.ht@gmail.com>
2022-07-22 16:36:09 -0700

    no

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,22 +6,6 @@ import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
     const [ingredients, setIngredients] = useState([])
-
-    useEffect(() => {
-        fetch('https://flamelinktest-b9226-default-rtdb.firebaseio.com/ingredients.json').then(
-            response => response.json()
-        ).then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-                loadedIngredients.push({
-                    id: key,
-                    title: responseData[key].title,
-                    amount: responseData[key].amount
-                });
-            }
-            setIngredients(loadedIngredients);
-        })
-    }, [])
 
     const filteredIngredientHandler = useCallback( filteredIngredients => {
         setIngredients(filteredIngredients)
